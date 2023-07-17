88ee5d7a6bbe4b4bdbff7d9b6df623da3bddd4ad
Gabriel Avila <aviladasilvag@gmail.com>
2023-07-16 22:35:02 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$return = array(
+	array(
+		"item" => "ab23a",
+		"category" => "drinks",
+		"name" => "Chopp 300ml",
+		"price" => "18.00",
+		"description" => "Chopp Pilsen geladinho"
+	),
+	array(
+		"item" => "asd5",
+		"category" => "drinks",
+		"name" => "Cocacola lata",
+		"price" => "18.00",
+		"description" => "Chopp Pilsen geladinho"
+	)
+);
+
+echo json_encode($return);
